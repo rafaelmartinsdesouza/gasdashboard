079c2a8d18c590f27d0ca857b2f6e1a25eee5bec
ddanieldma <rafaelmartinsdesouza@gmail.com>
2024-10-21 21:06:52 +0000

    Organizando CSS usado na calculadora.

--- calculadora_faixas/ui.R
+++ calculadora_faixas/ui.R
@@ -5,29 +5,6 @@ library(shiny)
 calculadora_faixas_ui <- function(id) {
   ns <- NS(id)
   tagList(
-    # Custom CSS for Flexbox layout.
-    tags$style(HTML("
-      .flex-container {
-        display: flex;
-        flex-wrap: wrap;
-        gap: 20px;
-        justify-content: center; /* Center tables horizontally */
-      }
-      .flex-item {
-        flex: 1 1 300px;
-        min-width: 300px;
-        max-width: 350px;
-        border: 1px solid #ddd;
-        padding: 10px;
-        margin-bottom: 20px;
-        text-align: center; /* Center text inside the flex item */
-      }
-      table {
-        margin-left: auto;
-        margin-right: auto;
-      }
-    ")),
-    
     titlePanel(
       h1("Estrutura tarifária da distribuição", align = "center")
     ),
